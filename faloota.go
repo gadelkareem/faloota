07b4988371bf3a21add29e195f6d475910af92d2
Waleed Gadelkareem <gadelkareem@gmail.com>
2019-04-06 07:25:46 +0200

    create unique keys for bypass once

--- faloota.go
+++ faloota.go
@@ -6,6 +6,7 @@ import (
 	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/chromedp"
 	"github.com/gadelkareem/cachita"
+	"github.com/gadelkareem/go-helpers"
 	"github.com/pkg/errors"
 	"net/http"
 	"sync"
@@ -13,7 +14,7 @@ import (
 )
 
 type Faloota struct {
-	*sync.RWMutex
+	*sync.Mutex
 	cache   cachita.Cache
 	cancels map[string]context.CancelFunc
 	ctxes   map[string]context.Context
@@ -21,8 +22,8 @@ type Faloota struct {
 
 func NewFaloota() (f *Faloota, err error) {
 	f = &Faloota{
-		RWMutex: &sync.RWMutex{},
-		cache:   cachita.NewMemoryCache(10*time.Minute, 10*time.Minute),
+		Mutex:   &sync.Mutex{},
+		cache:   cachita.NewMemoryCache(1*time.Hour, 1*time.Hour),
 		ctxes:   make(map[string]context.Context),
 		cancels: make(map[string]context.CancelFunc),
 	}
@@ -32,19 +33,20 @@ func NewFaloota() (f *Faloota, err error) {
 }
 
 func (f *Faloota) BypassOnce(inUrl, proxy, userAgent string, verify Action) (cookies []*http.Cookie, err error) {
-	cookies, err = f.Bypass(inUrl, proxy, userAgent, verify)
-	f.Cancel(proxy, userAgent)
+	id := h.RandomString(5)
+	cookies, err = f.Bypass(inUrl, proxy, userAgent, verify, id)
+	f.Cancel(proxy, userAgent, id)
 	return
 }
 
-func (f *Faloota) Bypass(inUrl, proxy, userAgent string, verify Action) (cookies []*http.Cookie, err error) {
-	cacheKey := cachita.Id(inUrl, proxy, userAgent)
+func (f *Faloota) Bypass(inUrl, proxy, userAgent string, verify Action, id ...string) (cookies []*http.Cookie, err error) {
+	cacheKey := key(inUrl, proxy, userAgent)
 	err = f.cache.Get(cacheKey, &cookies)
 	if len(cookies) > 0 {
 		return cookies, nil
 	}
 
-	ctx, err := f.Ctx(proxy, userAgent)
+	ctx, err := f.Ctx(proxy, userAgent, id...)
 	if err != nil {
 		return nil, err
 	}
@@ -87,11 +89,11 @@ func (f *Faloota) Bypass(inUrl, proxy, userAgent string, verify Action) (cookies
 	return cookies, err
 }
 
-func (f *Faloota) Ctx(proxy, userAgent string) (ctx context.Context, err error) {
+func (f *Faloota) Ctx(proxy, userAgent string, id ...string) (ctx context.Context, err error) {
 	f.Lock()
 	defer f.Unlock()
+	k := key(proxy, userAgent, id...)
 
-	k := cachita.Id(proxy, userAgent)
 	if ctx, ok := f.ctxes[k]; ok {
 		return ctx, nil
 	}
@@ -106,6 +108,7 @@ func (f *Faloota) Ctx(proxy, userAgent string) (ctx context.Context, err error) 
 			chromedp.Flag("no-default-browser-check", true),
 			chromedp.Flag("no-first-run", true),
 			chromedp.Flag("disable-fre", true),
+			chromedp.Flag("enable-native-notifications", false),
 			chromedp.Flag("window-position", "-999999,-999999"),
 			chromedp.Flag("window-size", "5,5"),
 			chromedp.Flag("user-agent", userAgent),
@@ -123,29 +126,38 @@ func (f *Faloota) Ctx(proxy, userAgent string) (ctx context.Context, err error) 
 }
 
 func (f *Faloota) Wait() {
-	f.RLock()
-	defer f.RUnlock()
-	for _, ctx := range f.ctxes {
+	f.Lock()
+	defer f.Unlock()
+	for k, ctx := range f.ctxes {
 		chromedp.FromContext(ctx).Allocator.Wait()
+		delete(f.ctxes, k)
 	}
 }
 
 func (f *Faloota) Close() {
-	f.RLock()
-	for _, cancel := range f.cancels {
+	f.Lock()
+	for k, cancel := range f.cancels {
 		cancel()
+		delete(f.cancels, k)
 	}
-	f.RUnlock()
+	f.Unlock()
 
 	f.Wait()
 }
 
-func (f *Faloota) Cancel(proxy, useragent string) {
-	f.RLock()
-	defer f.RUnlock()
-	k := cachita.Id(proxy, useragent)
-	f.cancels[k]()
-	delete(f.cancels, k)
+func (f *Faloota) Cancel(proxy, useragent string, id ...string) {
+	f.Lock()
+	defer f.Unlock()
+	k := key(proxy, useragent, id...)
+	if cancel, ok := f.cancels[k]; ok {
+		cancel()
+		delete(f.ctxes, k)
+		delete(f.cancels, k)
+	}
+}
+
+func key(proxy, userAgent string, id ...string) string {
+	return cachita.Id(proxy, userAgent, cachita.Id(id...))
 }
 
 type Action interface {
